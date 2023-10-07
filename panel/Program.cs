eacb45b2f9afcc8937fbe80401a5c2331e303679
F0RZENT <113331099+F0RZENT@users.noreply.github.com>
2023-10-07 10:09:45 +0500

    Add files via upload

---
+++ panel/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Xml.XPath;
+
+int prompt(string message) 
+{
+  System.Console.WriteLine(message);
+  int num = Int32.Parse(Console.ReadLine());
+  return num;
+}
+  void degree() 
+{
+  int num1 = prompt("Введите Степень: ");
+  int num2 = prompt("Введите Степень: ");
+  int result = 1;
+for (int i = 0; i < num2; i++)
+{
+  result *= num1;
+}
+Console.WriteLine(result);
+}
+degree();
